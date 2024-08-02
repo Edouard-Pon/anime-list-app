0ee34387e52115b6a43f396f97b96e8299992d40
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-08-02 05:42:47 +0200

    feat: add anime page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Anime from './pages/Anime'
 import NotFound from './pages/NotFound'
 import About from './pages/About.jsx'
 import Navigation from './components/Navigation.jsx'
@@ -11,6 +12,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/anime" element={<Anime />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>

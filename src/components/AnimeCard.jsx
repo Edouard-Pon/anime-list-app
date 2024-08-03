f4af41d7c2d30bf7804edab156ca18732551ce4d
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-08-03 02:12:37 +0200

    feat: add anime detail page

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,13 +1,13 @@
-import { getAnimeTitle, getAnimeCoverImage } from '../utils/animeUtils.js'
+import { Link } from 'react-router-dom';
+import { getAnimeTitle, getAnimeCoverImage, getAnimeId } from '../utils/animeUtils.js'
 import { animePropTypes } from '../props/animePropTypes.js';
 
 const AnimeCard = ({ anime }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg flex flex-col justify-between h-full">
-      <img className="w-full h-64 object-cover rounded-t-lg" src={getAnimeCoverImage(anime)}
-           alt={getAnimeTitle(anime)}/>
+    <Link to={`/anime/${getAnimeId(anime)}`} className="bg-white shadow-md rounded-lg flex flex-col justify-between h-full cursor-pointer">
+      <img className="w-full h-64 object-cover rounded-t-lg" src={getAnimeCoverImage(anime)} alt={getAnimeTitle(anime)}/>
       <h2 className="p-4 text-lg font-semibold text-gray-800">{getAnimeTitle(anime)}</h2>
-    </div>
+    </Link>
   )
 }
 

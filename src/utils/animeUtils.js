b097e50ca0266f0bb4813f2862b834a6bec07cb3
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-08-03 02:11:29 +0200

    feat: update utils
    - add formatDate
    - update animeUtils

--- src/utils/animeUtils.js
+++ src/utils/animeUtils.js
@@ -1,6 +1,19 @@
-export const getAnimeId = (anime) => anime._id
-export const getAnimeTitle = (anime) => anime.title
-export const getAnimeCoverImage = (anime) => anime.coverImagePath
+export const getAnimeId = (anime) => anime._id || null
+export const getAnimeTitle = (anime) => anime.title || null
+export const getAnimeCoverImage = (anime) => anime.coverImagePath || null
+export const getAnimeDescription = (anime) => anime.description || null
+export const getAnimeType = (anime) => anime.type || null
+export const getAnimeEpisodes = (anime) => anime.episodes || null
+export const getAnimeStatus = (anime) => anime.status || null
+export const getAnimeReleaseDate = (anime) => anime.releaseDate || null
+export const getAnimeUploadDate = (anime) => anime.uploadDate || null
+export const getAnimeSource = (anime) => anime.source || null
+export const getAnimeExternalLink = (anime) => anime.externalLink || null
+export const getAnimeDuration = (anime) => anime.duration || null
+export const getAnimeRating = (anime) => anime.rating || null
+export const getAnimeGenres = (anime) => anime.genres || []
+export const getAnimeThemes = (anime) => anime.themes || []
+export const getAnimeCharacters = (anime) => anime.characters || []
 
 export const buildAnimeFormData = (anime) => {
   const formData = new FormData()

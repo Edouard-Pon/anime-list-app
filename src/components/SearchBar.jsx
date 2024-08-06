3aabda139840d531b4c65246a1b69eaf2c372bf6
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-08-06 23:20:30 +0200

    feat: add search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,52 @@
+import { searchAnime } from '../store/anime'
+import { useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+const SearchBar = ({ setIsSearching }) => {
+  const dispatch = useDispatch()
+  const [searchType, setSearchType] = useState('anime')
+  const [searchQuery, setSearchQuery] = useState('')
+  const [debouncedQuery, setDebouncedQuery] = useState('')
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedQuery(searchQuery), 500)
+    return () => clearTimeout(handler)
+  }, [searchQuery])
+
+  useEffect(() => {
+    if (debouncedQuery.length >= 2) {
+      setIsSearching(true)
+      if (searchType === 'anime') dispatch(searchAnime({ title: debouncedQuery }))
+    } else {
+      setIsSearching(false)
+    }
+  }, [debouncedQuery, searchType, dispatch, setIsSearching])
+
+  const handleInputChange = (e) => setSearchQuery(e.target.value)
+
+  return (
+    <div className="flex items-center space-x-4">
+      <select
+        onChange={(e) => setSearchType(e.target.value)}
+        className="border border-gray-300 p-2 rounded"
+      >
+        <option value="anime">Anime</option>
+        <option value="characters">Characters</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchQuery}
+        onChange={handleInputChange}
+        className="border border-gray-300 p-2 rounded w-full"
+      />
+    </div>
+  )
+}
+
+SearchBar.propTypes = {
+  setIsSearching: PropTypes.func.isRequired
+}
+
+export default SearchBar
--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -6,34 +6,40 @@ import { getAnimeId } from '../utils/animeUtils.js'
 import { isAdmin } from '../utils/authUtils.js'
 import AnimeFormModal from '../components/AnimeFormModal'
 import Loading from '../components/Loading'
+import SearchBar from '../components/SearchBar'
 
 const Anime = () => {
   const user = useSelector((state) => state.auth.user)
   const animeList = useSelector((state) => state.anime.anime)
   const status = useSelector((state) => state.anime.status)
+  const searchStatus = useSelector((state) => state.anime.searchStatus)
   const dispatch = useDispatch()
   const [isModalOpen, setModalOpen] = useState(false)
+  const [isSearching, setIsSearching] = useState(false)
 
   useEffect(() => {
-    dispatch(fetchAnime())
-  }, [dispatch])
+    if (!isSearching) {
+      dispatch(fetchAnime())
+    }
+  }, [dispatch, isSearching])
 
-  if (status === 'loading') return <Loading />
   if (status === 'failed') return <div>Error fetching anime</div>
 
   return (
     <div className="w-9/12 bg-gray-100 m-auto">
-      <div>
-        <h1 className="text-3xl font-bold p-6">Anime</h1>
+      <div className="p-6">
+        <h1 className="text-3xl font-bold">Anime</h1>
         {user && isAdmin(user) && (
           <>
-            <button className="bg-blue-500 text-white rounded p-2 m-6" onClick={() => setModalOpen(true)}>
+            <button className="bg-blue-500 text-white rounded p-2 my-6" onClick={() => setModalOpen(true)}>
               Add Anime
             </button>
             <AnimeFormModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
           </>
         )}
+        <SearchBar setIsSearching={setIsSearching} />
       </div>
+      {((isSearching && searchStatus === 'loading') || status === 'loading') && <Loading />}
       <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
         {animeList.map(anime => (
           <AnimeCard key={getAnimeId(anime)} anime={anime}/>

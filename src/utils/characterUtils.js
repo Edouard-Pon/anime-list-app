f3394f0649f6758da5eaf4875e04b2eaa0e88adf
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-08-09 01:14:57 +0200

    feat: add character utils

---
+++ src/utils/characterUtils.js
@@ -0,0 +1,17 @@
+export const getCharacterId = (character) => character._id || null
+export const getCharacterName = (character) => character.name || null
+export const getCharacterOriginalName = (character) => character.originalName || null
+export const getCharacterDescription = (character) => character.description || null
+export const getCharacterImage = (character) => character.imagePath || null
+export const getCharacterAnime = (character) => character.anime || []
+export const getCharacterUploadDate = (character) => character.uploadDate || null
+
+export const buildCharacterFormData = (character) => {
+  const formData = new FormData()
+  formData.append('name', character.name)
+  formData.append('originalName', character.originalName)
+  formData.append('description', character.description)
+  formData.append('image', character.image)
+  formData.append('anime', character.anime)
+  return formData
+}

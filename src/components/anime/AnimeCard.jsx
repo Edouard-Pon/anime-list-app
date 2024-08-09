f37966dcbfbd06366e1985de0ee29d78f7f5f80f
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-08-09 04:59:12 +0200

    style: update anime card

--- src/components/anime/AnimeCard.jsx
+++ src/components/anime/AnimeCard.jsx
@@ -4,8 +4,8 @@ import { animePropTypes } from '../../props/animePropTypes.js';
 
 const AnimeCard = ({ anime }) => {
   return (
-    <Link to={`/anime/${getAnimeId(anime)}`} className="bg-white shadow-md rounded-lg flex flex-col justify-between h-full cursor-pointer">
-      <img className="w-full h-64 object-cover rounded-t-lg" src={getAnimeCoverImage(anime)} alt={getAnimeTitle(anime)}/>
+    <Link to={`/anime/${getAnimeId(anime)}`} className="w-52 bg-white shadow-md rounded-lg flex flex-col justify-between cursor-pointer">
+      <img className="h-64 object-cover rounded-t-lg" src={getAnimeCoverImage(anime)} alt={getAnimeTitle(anime)}/>
       <h2 className="p-4 text-lg font-semibold text-gray-800">{getAnimeTitle(anime)}</h2>
     </Link>
   )

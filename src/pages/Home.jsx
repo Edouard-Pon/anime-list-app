fb174d306f0d6186e2b6a72843411173ea7c70aa
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-08-09 05:11:50 +0200

    style: update style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,12 @@ const Home = () => {
   if (status === 'failed') return <div>Error fetching anime</div>
 
   return (
-    <div className="m-auto w-9/12 p-6 bg-gray-100 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
-      {animeList.map(anime => (
-        <AnimeCard key={getAnimeId(anime)} anime={anime}/>
-      ))}
+    <div className="container m-auto p-6 bg-gray-100">
+      <div className="grid justify-items-center gap-6" style={{ gridTemplateColumns: "repeat(auto-fit, minmax(208px, 1fr))" }}>
+        {animeList.map(anime => (
+          <AnimeCard key={getAnimeId(anime)} anime={anime} />
+        ))}
+      </div>
     </div>
   )
 }

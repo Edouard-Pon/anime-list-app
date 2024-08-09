db4753f30f3d059f228f3f702f36b1a1974cba4e
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-08-09 05:00:02 +0200

    feat: add character detail page

---
+++ src/pages/CharacterDetail.jsx
@@ -0,0 +1,81 @@
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCharacterById, resetSelectedStatus } from '../store/characters'
+import Loading from '../components/Loading'
+import { formatDate } from '../utils/utils'
+import {
+  getCharacterId,
+  getCharacterAnime,
+  getCharacterName,
+  getCharacterImage,
+  getCharacterUploadDate,
+  getCharacterOriginalName,
+  getCharacterDescription,
+} from '../utils/characterUtils';
+import AnimeCard from '../components/anime/AnimeCard.jsx';
+import {getAnimeId} from '../utils/animeUtils.js';
+
+const CharacterDetail = () => {
+  const { id } = useParams()
+  const character = useSelector((state) => state.characters.selectedCharacter)
+  const animeList = useSelector((state) => state.characters.selectedCharacterAnime)
+  const status = useSelector((state) => state.characters.selectedStatus)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchCharacterById(id))
+  }, [id, dispatch])
+
+  useEffect(() => {
+    if (status === 'succeeded') {
+      dispatch(resetSelectedStatus())
+    }
+  }, [status, dispatch])
+
+  if (status === 'loading') return <Loading />
+  if (status === 'failed') return <div>Error fetching character</div>
+
+  return (
+    <div className="container mx-auto p-4 bg-gray-100 flex flex-wrap gap-6">
+      <div className="w-64">
+        <div className="w-64">
+          <img className="rounded-lg object-cover" src={getCharacterImage(character)} alt={getCharacterName(character)}/>
+        </div>
+        <div className="flex flex-col gap-1 p-2 mt-6 bg-gray-200 rounded-lg">
+          <div className="px-3 py-1 rounded-lg hover:bg-gray-100">
+            <p className="text-xs">Name</p>
+            <p className="text-lg">{getCharacterName(character)}</p>
+          </div>
+          {getCharacterOriginalName(character) && (
+            <div>
+              <p>Original Name</p>
+              <p>{getCharacterOriginalName(character)}</p>
+            </div>
+          )}
+          <div>
+            <p>Upload Date</p>
+            <p>{formatDate(getCharacterUploadDate(character))}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-grow bg-gray-200 rounded-lg p-4">
+        <div className="mb-4 flex justify-between">
+          <h1 className="text-3xl">{getCharacterName(character)}</h1>
+        </div>
+        {getCharacterDescription(character) && (
+          <div className="rounded-lg bg-gray-300 p-4">
+            <p className="text-sm">{getCharacterDescription(character)}</p>
+          </div>
+        )}
+        <div className="mt-6 flex flex-wrap gap-6">
+          {animeList.map(anime => (
+            <AnimeCard key={getAnimeId(anime)} anime={anime}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CharacterDetail

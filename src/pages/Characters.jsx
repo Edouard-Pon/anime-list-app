7407de3cb2503bfda16cf8be7caa0785bc78b793
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-08-09 07:48:18 +0200

    feat: add button in characters page

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -3,14 +3,18 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchCharacters } from '../store/characters.js'
 import CharacterCard from '../components/character/CharacterCard'
 import { getCharacterId } from '../utils/characterUtils'
+import { isAdmin } from '../utils/authUtils'
+import CharacterFormModal from '../components/character/CharacterFormModal'
 import Loading from '../components/Loading'
 import SearchBar from '../components/SearchBar'
 
 const Characters = () => {
+  const user = useSelector((state) => state.auth.user)
   const charactersList = useSelector((state) => state.characters.characters)
   const status = useSelector((state) => state.characters.status)
   const searchStatus = useSelector((state) => state.characters.searchStatus)
   const dispatch = useDispatch()
+  const [isModalOpen, setModalOpen] = useState(false)
   const [isSearching, setIsSearching] = useState(false)
 
   useEffect(() => {
@@ -25,6 +29,14 @@ const Characters = () => {
     <div className="container bg-gray-100 m-auto">
       <div className="p-6">
         <h1 className="text-3xl font-bold">Characters</h1>
+        {user && isAdmin(user) && (
+          <>
+            <button className="bg-blue-500 text-white rounded p-2 my-6" onClick={() => setModalOpen(true)}>
+              Add Character
+            </button>
+            <CharacterFormModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
+          </>
+        )}
         <SearchBar setIsSearching={setIsSearching} />
       </div>
       {((isSearching && searchStatus === 'loading') || status === 'loading') && <Loading />}

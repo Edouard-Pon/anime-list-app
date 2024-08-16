33df2929d4353d7ccd70545088f126c08d3ad3f3
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-08-16 05:52:49 +0200

    feat: add findAnimeListCurrentStatus to animeListUtils

--- src/utils/animeListUtils.js
+++ src/utils/animeListUtils.js
@@ -3,3 +3,13 @@ export const getAnimeListToWatch = (animeList) => animeList.toWatch || []
 export const getAnimeListWatched = (animeList) => animeList.watched || []
 export const getAnimeListAbandoned = (animeList) => animeList.abandoned || []
 export const getAnimeListAnime = (item) => item.animeId || null
+
+export const findAnimeListCurrentStatus = (animeId, animeList) => {
+  const { toWatch, watched, abandoned } = animeList
+
+  if (toWatch.some(anime => anime.animeId._id === animeId)) return 'toWatch'
+  if (watched.some(anime => anime.animeId._id === animeId)) return 'watched'
+  if (abandoned.some(anime => anime.animeId._id === animeId)) return 'abandoned'
+
+  return 'default'
+}

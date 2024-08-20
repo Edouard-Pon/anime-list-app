fa1f997614a2814e3414d74aeef4241534d6eb03
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2024-08-20 04:46:48 +0200

    feat: add watching to animeListUtils

--- src/utils/animeListUtils.js
+++ src/utils/animeListUtils.js
@@ -1,15 +1,17 @@
 export const getAnimeListFavorites = (animeList) => animeList.favorites || []
+export const getAnimeListWatching = (animeList) => animeList.watching || []
 export const getAnimeListToWatch = (animeList) => animeList.toWatch || []
 export const getAnimeListWatched = (animeList) => animeList.watched || []
 export const getAnimeListAbandoned = (animeList) => animeList.abandoned || []
 export const getAnimeListAnime = (item) => item.animeId || null
 
 export const findAnimeListCurrentStatus = (animeId, animeList) => {
-  const { toWatch, watched, abandoned } = animeList
+  const { toWatch, watched, abandoned, watching } = animeList
 
   if (toWatch.some(anime => anime.animeId._id === animeId)) return 'toWatch'
   if (watched.some(anime => anime.animeId._id === animeId)) return 'watched'
   if (abandoned.some(anime => anime.animeId._id === animeId)) return 'abandoned'
+  if (watching.some(anime => anime.animeId._id === animeId)) return 'watching'
 
   return 'default'
 }
